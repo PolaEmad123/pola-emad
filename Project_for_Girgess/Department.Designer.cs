617248e1e48445db4b143b9d4f533745469cc22a
polaemad##012 <emadpola93@gmail.com>
2023-01-27 14:15:02 +0200

    add this.label1.Location

--- Project_for_Girgess/Department.Designer.cs
+++ Project_for_Girgess/Department.Designer.cs
@@ -189,3 +189,4 @@
         this.label1.BackColor = System.Drawing.Color.White;
         this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
         this.label1.ForeColor = System.Drawing.Color.Teal;
+        this.label1.Location = new System.Drawing.Point(373, 121);
