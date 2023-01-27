a2c8374fda7e2f3323f68458ecb702e2511e32f4
polaemad##012 <emadpola93@gmail.com>
2023-01-27 15:37:47 +0200

    add this.label3.Text

--- Project_for_Girgess/Department.Designer.cs
+++ Project_for_Girgess/Department.Designer.cs
@@ -216,3 +216,4 @@
         this.label3.Name = "label3";
         this.label3.Size = new System.Drawing.Size(147, 22);
         this.label3.TabIndex = 40;
+        this.label3.Text = " Department List";
