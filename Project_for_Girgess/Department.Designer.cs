ab34e1e226ed36ea67027c14fc32299891370203
polaemad##012 <emadpola93@gmail.com>
2023-01-27 11:53:46 +0200

    add this.AddBtn

--- Project_for_Girgess/Department.Designer.cs
+++ Project_for_Girgess/Department.Designer.cs
@@ -33,3 +33,4 @@
         System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
         System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Department));
         this.label8 = new System.Windows.Forms.Label();
+        this.AddBtn = new System.Windows.Forms.Button();
