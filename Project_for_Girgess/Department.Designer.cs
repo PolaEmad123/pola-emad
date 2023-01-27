4e6bdf9511d8e620112c743d18ceb667ae2c0d1a
polaemad##012 <emadpola93@gmail.com>
2023-01-27 12:30:02 +0200

    add label8.ForeColor

--- Project_for_Girgess/Department.Designer.cs
+++ Project_for_Girgess/Department.Designer.cs
@@ -66,4 +66,4 @@
         this.label8.AutoSize = true;
         this.label8.BackColor = System.Drawing.Color.Teal;
         this.label8.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      
+        this.label8.ForeColor = System.Drawing.SystemColors.Control;
