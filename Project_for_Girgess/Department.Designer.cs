4132f3c8083b8603eaee3e14f84b8ca6e526fd0b
polaemad##012 <emadpola93@gmail.com>
2023-01-27 14:07:39 +0200

    this.pictureBox1.Size

--- Project_for_Girgess/Department.Designer.cs
+++ Project_for_Girgess/Department.Designer.cs
@@ -178,3 +178,4 @@
         this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
         this.pictureBox1.Location = new System.Drawing.Point(12, 65);
         this.pictureBox1.Name = "pictureBox1";
+        this.pictureBox1.Size = new System.Drawing.Size(164, 78);
