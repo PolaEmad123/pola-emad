d7ca665feb0cdb1016a79299b25c087d17dadf85
polaemad##012 <emadpola93@gmail.com>
2023-01-27 12:43:29 +0200

    add  this.AddBtn.Location

--- Project_for_Girgess/Department.Designer.cs
+++ Project_for_Girgess/Department.Designer.cs
@@ -78,5 +78,6 @@
         //
         this.AddBtn.BackColor = System.Drawing.Color.Teal;
         this.AddBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-          this.AddBtn.Font = new System.Drawing.Font("Rockwell", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.AddBtn.ForeColor = System.Drawing.Color.BlanchedAlmond;
+        this.AddBtn.Font = new System.Drawing.Font("Rockwell", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+        this.AddBtn.ForeColor = System.Drawing.Color.BlanchedAlmond;
+        this.AddBtn.Location = new System.Drawing.Point(12, 225);
