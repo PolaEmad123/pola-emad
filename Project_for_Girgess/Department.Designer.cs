1511525de376f5b69bab05033db465aff9f1d6fe
polaemad##012 <emadpola93@gmail.com>
2023-01-27 14:17:25 +0200

    add  this.label1.Size

--- Project_for_Girgess/Department.Designer.cs
+++ Project_for_Girgess/Department.Designer.cs
@@ -192,3 +192,4 @@
         this.label1.Location = new System.Drawing.Point(373, 121);
         this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
         this.label1.Name = "label1";
+        this.label1.Size = new System.Drawing.Size(177, 22);
