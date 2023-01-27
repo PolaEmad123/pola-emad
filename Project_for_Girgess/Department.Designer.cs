29370615851539bed3fc33ced5d257cca9542373
polaemad##012 <emadpola93@gmail.com>
2023-01-27 11:35:17 +0200

    add code

--- Project_for_Girgess/Department.Designer.cs
+++ Project_for_Girgess/Department.Designer.cs
@@ -9,3 +9,4 @@
           /// <summary>
           /// Clean up any resources being used.
           /// </summary>
+          /// <param name="disposing">true if managed resources should be disposed;
